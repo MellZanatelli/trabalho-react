89aabb287544b46d7aa89e7d33fba3f636dfca65
MellZanatelli <mell.zanatelli@gmail.com>
2023-08-04 08:55:20 -0300

    aula 04/08

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom"
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { RouteSoftware } from "./routes";
+
+export function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+        <RouteSoftware />
+      </BrowserRouter>
+    </>
+  )
+}
+
